fbdd90aca75889f013349987f8001a9bf089414d
Nalanda Souza <nalanda.ue@gmail.com>
2025-07-25 19:17:53 -0300

    add dotenv

--- api/db.js
+++ api/db.js
@@ -1,9 +1,12 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createPool({
-  host: "localhost",
-  port: "3307",
-  user: "root",
-  password: "",
-  database: "blog",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
